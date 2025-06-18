c2f943383b18fd7e104f5b7173335b7f1ff3b2ae
dhyabi2 <32069256+dhyabi2@users.noreply.github.com>
2025-06-18 13:11:10 +0400

    Add provider skills page and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Home from "./pages/Home";
 import Browse from "./pages/Browse";
 import Bookings from "./pages/Bookings";
 import Profile from "./pages/Profile";
+import MySkills from "./pages/MySkills";
 import Notifications from "./pages/Notifications";
 import SkillDetail from "./pages/SkillDetail";
 import BookingConfirmation from "./pages/BookingConfirmation";
@@ -54,6 +55,7 @@ const AppContent: React.FC = () => {
                 <Route path="bookings" element={<Bookings />} />
                 <Route path="notifications" element={<Notifications />} />
                 <Route path="profile" element={<Profile />} />
+                <Route path="my-skills" element={<MySkills />} />
                 <Route path="skill/:id" element={<SkillDetail />} />
                 <Route path="booking/create" element={<BookingCreate />} />
                 <Route path="booking/:id" element={<BookingConfirmation />} />
